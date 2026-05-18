bd95187726d7a8471388b85f19e5d0db158098bf
Ricardo Uzhca <ricardouzhcab27@gmail.com>
2026-05-18 07:54:28 -0500

    ADD: Metodos Busqueda

--- src/BusquedaBinaria.java
+++ src/BusquedaBinaria.java
@@ -2,7 +2,24 @@ import models.Product;
 
 public class BusquedaBinaria {
 
-    public int findByName(Product [] products){
-        return -1;
+    public int findByName(Product [] products, String name) {
+        //definamos el punto medio
+        //comparemos con la clave
+        //si es igual, retornamos el punto medio
+        //si es mayor, actualizamos el index correspondiente
+        //si es menor, actualizamos el index correspondiente
+        int bajo = 0;
+        int alto = products.length - 1;
+
+        while (bajo <= alto) {
+            int central = (bajo + alto) / 2;
+            if (products[central].getName().equals(name)) {
+                return central; // Encontrado
+            } else if (products[central].getName().compareTo(name) < 0) {
+                bajo = central + 1; // Buscar en la mitad superior
+            } else {
+                alto = central - 1; // Buscar en la mitad inferior
+            }
+        }   
     }
 }
