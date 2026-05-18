833d0d641915c0c4ef6eac86b388883e43f0d7ac
Ricardo Uzhca <ricardouzhcab27@gmail.com>
2026-05-18 08:22:01 -0500

    ADD: Metodos Busqueda

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import models.Product;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Product[] products = {
+            new Product("Laptop", 20),
+            new Product("Smartphone", 25),
+            new Product("Tablet", 10),
+            new Product("Monitor", 5),
+            new Product("Keyboard",15)
+        };
+        // Ordenar el arreglo de productos por nombre
+        BusquedaBinaria bBinaria = new BusquedaBinaria();
+        bBinaria.sortByName(products);
+        for (Product product : products) {
+            System.out.println(product);
+        }
+        int res = bBinaria.findByName(products, "Laptop");
+        if (res >= 0) {
+            System.out.println("Entre en pos: " + res);
+        } else {
+            System.out.println("No se encontró");
+        }
     }
 }
--- src/BusquedaBinaria.java
+++ src/BusquedaBinaria.java
@@ -2,24 +2,49 @@ import models.Product;
 
 public class BusquedaBinaria {
 
-    public int findByName(Product [] products, String name) {
+    public void sortByName(Product[] products) {
+        // Ordenar el arreglo de productos por nombre utilizando el método de burbuja
+        for (int i = 0; i < products.length - 1; i++) {
+            boolean swap = false;
+            for (int j = 0; j < products.length - i - 1; j++) {
+                if (products[j].getName().compareTo(products[j + 1].getName()) > 0) {
+                    // Intercambiar productos[j] y productos[j + 1]
+                    Product temp = products[j];
+                    products[j] = products[j + 1];
+                    products[j + 1] = temp;
+                    swap = true;
+                }
+            }
+            if (!swap) {
+                break;
+            }
+        }
+    }
+
+    public int findByName(Product [] products, String nombre) {
         //definamos el punto medio
         //comparemos con la clave
         //si es igual, retornamos el punto medio
         //si es mayor, actualizamos el index correspondiente
         //si es menor, actualizamos el index correspondiente
+
         int bajo = 0;
         int alto = products.length - 1;
 
         while (bajo <= alto) {
             int central = (bajo + alto) / 2;
-            if (products[central].getName().equals(name)) {
+            if (products[central].getName().equals(nombre)) {
                 return central; // Encontrado
-            } else if (products[central].getName().compareTo(name) < 0) {
+            } 
+            //si voy a la derecha o izquierda
+            int comparacion = products[central].getName().compareTo(nombre);
+            if (comparacion < 0) {
                 bajo = central + 1; // Buscar en la mitad superior
             } else {
                 alto = central - 1; // Buscar en la mitad inferior
             }
         }   
+    
+        return -1; // No encontrado
     }
 }
