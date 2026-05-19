fcf09ac73d06b941012362ceec3a00fd6696ebc3
Ricardo Uzhca <ricardouzhcab27@gmail.com>
2026-05-19 00:05:24 -0500

    UPD: Commit final

--- src/App.java
+++ src/App.java
@@ -10,30 +10,63 @@ public class App {
         int cont = 0;
 
         while (cont < 9) {
-            System.out.println("Ingrese el nombre -->");
+            System.out.print("Ingrese el nombre --> ");
             String nombre = teclado.nextLine();
-            System.out.println("Ingrese la edad -->");
-            int edad = teclado.nextInt();
+
+            int edad = -1;
+            while (edad < 0) {
+                System.out.print("Ingrese la edad --> ");
+                edad = teclado.nextInt();
+                teclado.nextLine();
+                if (edad < 0) {
+                    System.out.print("Edad incorrecta");
+                }
+            }
+
             personas[cont] = new Persona(nombre, edad);
             cont++;
         }
 
         // Ordenar el arreglo de productos por nombre
         BusquedaBinaria bBinaria = new BusquedaBinaria();
-        bBinaria.sortByAge(personas);
-        for (Persona persona : personas) {
-            System.out.println(persona);
-        }
 
+        System.out.print("\nIngrese la forma de buscar (1. Por nombre, 2. Por edad) --> ");
+        int opcion = teclado.nextInt();
+        teclado.nextLine();
 
-        System.out.println("Ingrese la edad a buscar --> ");
-        int edadBuscar = teclado.nextInt();
-        int res = bBinaria.findByAge(personas, edadBuscar);
+        if (opcion == 1) {
+            bBinaria.sortByName(personas);
+            System.out.println("\nArreglo ordenado por nombre:");
+            for (Persona persona : personas) {
+                System.out.println(persona);
+            }
+            System.out.print("\nIngrese el nombre a buscar --> ");
+            String nombreBuscar = teclado.nextLine();
 
-        if (res >= 0) {
-            System.out.println("Entre en pos: " + res);
+            System.out.println();
+            int res = bBinaria.findByName(personas, nombreBuscar);
+
+            if (res >= 0) {
+                System.out.println("Entre en pos: " + res);
+            } else {
+                System.out.println("No se encontró");
+            }
+
         } else {
-            System.out.println("No se encontró");
+            bBinaria.sortByAge(personas);
+            System.out.println("\nArreglo ordenado por edad:");
+            for (Persona persona : personas) {
+                System.out.println(persona);
+            }
+            System.out.print("\nIngrese la edad a buscar --> ");
+            int edadBuscar = teclado.nextInt();
+            teclado.nextLine();
+            int res = bBinaria.findByAge(personas, edadBuscar);
+            if (res >= 0) {
+                System.out.println("Entre en pos: " + res);
+            } else {
+                System.out.println("No se encontró");
+            }
         }
     }
 }
--- src/BusquedaBinaria.java
+++ src/BusquedaBinaria.java
@@ -2,16 +2,14 @@ import models.Persona;
 
 public class BusquedaBinaria {
 
-    public void sortByAge(Persona[] products) {
-        // Ordenar el arreglo de productos por nombre utilizando el método de burbuja
-        for (int i = 0; i < products.length - 1; i++) {
+    public void sortByAge(Persona[] personas) {
+        for (int i = 0; i < personas.length - 1; i++) {
             boolean swap = false;
-            for (int j = 0; j < products.length - i - 1; j++) {
-                if (products[j].getEdad() > products[j + 1].getEdad()) {
-                    // Intercambiar productos[j] y productos[j + 1]
-                    Persona temp = products[j];
-                    products[j] = products[j + 1];
-                    products[j + 1] = temp;
+            for (int j = 0; j < personas.length - i - 1; j++) {
+                if (personas[j].getEdad() > personas[j + 1].getEdad()) {
+                    Persona temp = personas[j];
+                    personas[j] = personas[j + 1];
+                    personas[j + 1] = temp;
                     swap = true;
                 }
             }
@@ -21,7 +19,24 @@ public class BusquedaBinaria {
         }
     }
 
-    public int findByAge(Persona [] products, int edad) {
+    public void sortByName(Persona[] personas) {
+        for (int i = 0; i < personas.length - 1; i++) {
+            boolean swap = false;
+            for (int j = 0; j < personas.length - i - 1; j++) {
+                if (personas[j].getNombre().compareTo(personas[j + 1].getNombre()) > 0) {
+                    Persona temp = personas[j];
+                    personas[j] = personas[j + 1];
+                    personas[j + 1] = temp;
+                    swap = true;
+                }
+            }
+            if (!swap) {
+                break;
+            }
+        }
+    }
+
+    public int findByAge(Persona [] personas, int edad) {
         //definamos el punto medio
         //comparemos con la clave
         //si es igual, retornamos el punto medio
@@ -29,22 +44,74 @@ public class BusquedaBinaria {
         //si es menor, actualizamos el index correspondiente
 
         int bajo = 0;
-        int alto = products.length - 1;
+        int alto = personas.length - 1;
 
         while (bajo <= alto) {
             int central = (bajo + alto) / 2;
-            if (products[central].getEdad() == edad) {
-                return central; // Encontrado
+
+            for (int i = bajo; i <= alto; i++) {
+                System.out.print(personas[i].getEdad());
+                if (i<alto) {
+                    System.out.print(" | ");
+                }
+            }
+            System.out.print("\n");
+
+            System.out.print("bajo="+bajo+" alto="+alto+" central="+central+" valorCentro="+personas[central].getEdad()+ " --> ");
+            
+            if (personas[central].getEdad() == edad) {
+                System.out.println("ENCONTRADO");
+                return central;
             } 
             //si voy a la derecha o izquierda
-            int comparacion = products[central].getEdad() - edad;
+            int comparacion = personas[central].getEdad() - edad;
             if (comparacion < 0) {
-                bajo = central + 1; // Buscar en la mitad superior
+                System.out.println("DERECHA");
+                bajo = central + 1;
             } else {
-                alto = central - 1; // Buscar en la mitad inferior
+                System.out.println("IZQUIERDA");
+                alto = central - 1;
             }
         }   
     
-        return -1; // No encontrado
+        return -1; // Para no encontrado
     }
+
+    public int findByName(Persona [] personas, String nombre) {
+        int bajo = 0;
+        int alto = personas.length - 1;
+
+        while (bajo <= alto) {
+            int central = (bajo + alto) / 2;
+
+            for (int i = bajo; i <= alto; i++) {
+                System.out.print(personas[i].getNombre());
+                if (i<alto) {
+                    System.out.print(" | ");
+                }
+            }
+            System.out.print("\n");
+
+            System.out.print("bajo="+bajo+" alto="+alto+" central="+central+" valorCentro="+personas[central].getNombre()+ " --> ");
+
+            if (personas[central].getNombre().equals(nombre)) {
+                System.out.println("ENCONTRADO");
+                return central; // Encontrado
+            } 
+            //si voy a la derecha o izquierda
+            int comparacion = personas[central].getNombre().compareTo(nombre);
+            if (comparacion < 0) {
+                System.out.println("DERECHA");
+                bajo = central + 1;
+            } else {
+                System.out.println("IZQUIERDA");
+                alto = central - 1;
+            }
+        }   
+    
+        return -1;
+    }
+
+    
+
 }
